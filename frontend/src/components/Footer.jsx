91ee093f8ed98d87aab6717812f4e1f815b02413
VPeneva <victoria.peneva2007@gmail.com>
2025-11-15 19:51:42 +0200

    changes to footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -16,8 +16,8 @@ function Copyright() {
   return (
     <Typography variant="body2" sx={{ color: 'text.secondary', mt: 1 }}>
       {'Copyright © '}
-      <Link color="text.secondary" href="https://mui.com/">
-        Sitemark
+      <Link color="text.secondary" href="https://mgpz.org/">
+        MGPZ.org
       </Link>
       &nbsp;
       {new Date().getFullYear()}

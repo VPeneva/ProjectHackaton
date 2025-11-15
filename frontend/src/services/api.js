cacca5e5702a1e5dd3408cd1aadb313e048f9f55
danbso <daniel.b.sokolov@gmail.com>
2025-11-15 18:35:35 +0200

    added api.js back to tree WITH localhost as default.

---
+++ frontend/src/services/api.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://localhost:5000/api" // - за локално тестване
+  //baseURL: "http://172.20.10.3:5000/api" //- само за хотспот-а
+});
+
+// автоматично добавяне на token към заявките
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
+
+export default api;

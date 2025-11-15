350924f857fccbf80838c1763aa689f9d93e5c64
ItzGosho <itzgosho@gmail.com>
2025-11-15 11:46:19 +0200

    change db to have roles, add a way to register an admin account

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -3,7 +3,7 @@ import jwt from "jsonwebtoken";
 import { hashPassword, comparePassword } from "../utils/hash.js";
 
 export const register = async (req, res) => {
-  const { email, password, name } = req.body;
+  const { email, password, name, adminKey } = req.body;
 
   try {
     const exists = await prisma.user.findUnique({ where: { email } });
@@ -11,8 +11,15 @@ export const register = async (req, res) => {
 
     const hashed = await hashPassword(password);
 
+    // Избор на роля
+    let role = "USER";
+
+    if (adminKey && adminKey === process.env.ADMIN_REGISTER_KEY) {
+      role = "ADMIN";
+    }
+
     const user = await prisma.user.create({
-      data: { email, password: hashed, name }
+      data: { email, password: hashed, name, role }
     });
 
     res.json({ message: "Registered successfully", user });
@@ -20,6 +27,7 @@ export const register = async (req, res) => {
     res.status(500).json({ error: "Server error" });
   }
 };
+
 
 export const login = async (req, res) => {
   const { email, password } = req.body;
